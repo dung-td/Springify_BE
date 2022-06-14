41b9063750d161616e68a2b1fe24691af52ca45c
DucDung_Tong <tngcdng@gmail.com>
2022-06-14 22:29:54 +0700

    config heroku deploy

--- src/main/java/com/training/MusicPlayer/MusicPlayerApplication.java
+++ src/main/java/com/training/MusicPlayer/MusicPlayerApplication.java
@@ -2,7 +2,6 @@ package com.training.MusicPlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication()
