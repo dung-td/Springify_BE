42c3bbebb78caaf779b1e6ee99c1313024b3e7b9
DucDung_Tong <tngcdng@gmail.com>
2022-06-18 22:56:55 +0700

    Fix bugs

--- src/main/java/com/training/MusicPlayer/WebSecurityConfig.java
+++ src/main/java/com/training/MusicPlayer/WebSecurityConfig.java
@@ -18,6 +18,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -54,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().and()
+                .cors().and()
                 .authorizeRequests()
                     .antMatchers(
                             "/api/login",
@@ -62,10 +70,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             "/api/music/get",
                             "/api/music/page",
                             "/api/music/getRelated").permitAll()
-                    .anyRequest().authenticated()
-                .and().csrf().disable();
+                    .anyRequest().authenticated().and().csrf().disable();
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
+                "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type",
+                "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
 
